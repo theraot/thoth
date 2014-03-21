1cfdd156526b57ec9a6861761d4beae5adf144c1
= <theraot@gmail.com>
2014-03-21 03:43:38 -0500

    Added String.format

--- thoth.js
+++ thoth.js
@@ -2,6 +2,31 @@
 
 'use strict';
 
+(	/* strings */
+	function(thoth, window, undefined)
+	{
+		if (!String.format)
+		{
+			String.format = function(format)
+			{
+				if (this === void 0 || this === null)
+				{
+					throw new TypeError();
+				}
+				var args = Array.prototype.slice.call(arguments, 1);
+				return String.prototype.replace.call (
+					format,
+					/{(\d+)}/g,
+					function(match, number)
+					{
+						return number in args ? args[number] : match;
+					}
+				);
+			};
+		}
+	}
+)(window.thoth = (window.thoth || {}), window);
+
 (	/* arrays */
 	function(thoth, window, undefined)
 	{
